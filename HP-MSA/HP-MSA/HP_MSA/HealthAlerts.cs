3f574f57f681e8449860037ecd88b046fb3f391a
Paul Maurais <pmaurais@gmail.com>
2017-12-12 13:03:36 -0500

    Working SQL Query

---
+++ HP-MSA/HP-MSA/HP_MSA/HealthAlerts.cs
@@ -0,0 +1,51 @@
+﻿// +JMJ+
+// Paul A Maurais
+
+using System;
+using System.Text; 
+using System.IO;
+using System.Net;
+
+namespace HP_MSA
+{
+    public class HealthAlerts
+    {
+
+
+        private static String checkStatus()
+        {
+            HttpWebRequest request=(HttpWebRequest)HttpWebRequest.Create (new Uri ("http://54.173.140.216:8080"));
+            
+            var postData = "query=SELECT * FROM users";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+
+        }
+
+
+        public static String alert()
+        {
+            return checkStatus();
+        }
+               
+        
+        
+    }
+}
+
+
+//+JMJ+
