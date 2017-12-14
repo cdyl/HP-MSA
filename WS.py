1e08d8fc1ac43b38bf55be0456b30eea9d9ae9b5
pmaurais <pmaurais@gmail.com>
2017-12-14 11:04:29 -0500

    Web Server Source Code

---
+++ WS.py
@@ -0,0 +1,56 @@
+#+JMJ+
+#Paul A Maurais
+
+import cherrypy
+import mysql.connector
+import json
+
+cnx = mysql.connector.connect(user='gordon', password='password',
+                              host='msa.cz0sfiru3pto.us-east-1.rds.amazonaws.com',
+                              database='msa')
+
+
+
+@cherrypy.expose
+class StringGeneratorWebService(object):
+
+
+
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self):
+        return "NO FXN"
+
+    def POST(self, query):
+        responseArr=list()
+        cursor = cnx.cursor()
+        cursor.execute(query)
+
+        for (item) in cursor:
+            responseArr.append(item)
+
+        return json.dumps(responseArr)
+        cursor.close()
+
+    def PUT(self, another_string):
+        return "NO FXN"
+
+    def DELETE(self):
+        return "NO FXN"
+
+
+if __name__ == '__main__':
+
+
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        }
+    }
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    cherrypy.quickstart(StringGeneratorWebService(), '/', conf)
+
+
+# +JMJ+
