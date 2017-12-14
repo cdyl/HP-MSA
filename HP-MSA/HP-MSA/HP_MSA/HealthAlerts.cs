22f7a95842a7ab4bfea5ee4a95f33260c6a38008
pmaurais <pmaurais@gmail.com>
2017-12-13 23:08:42 -0500

    Halth Alert backend

--- HP-MSA/HP-MSA/HP_MSA/HealthAlerts.cs
+++ HP-MSA/HP-MSA/HP_MSA/HealthAlerts.cs
@@ -5,6 +5,9 @@ using System;
 using System.Text; 
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HP_MSA
 {
@@ -12,11 +15,11 @@ namespace HP_MSA
     {
 
 
-        private static String checkStatus()
+        private static String getData(String companyName)
         {
             HttpWebRequest request=(HttpWebRequest)HttpWebRequest.Create (new Uri ("http://54.173.140.216:8080"));
             
-            var postData = "query=SELECT * FROM users";
+            var postData = "query=SELECT `capacity.total.freePct`, systemName FROM data WHERE companyName='"+companyName+"'";
             var data = Encoding.ASCII.GetBytes(postData);
 
             request.Method = "POST";
@@ -32,14 +35,31 @@ namespace HP_MSA
 
             var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
 
-            return responseString;
+            return responseString.Substring(1,responseString.Length-1);
 
         }
 
 
-        public static String alert()
+        public static List<List<String>> alert(String companyName)
         {
-            return checkStatus();
+            String rawData = getData(companyName);
+            List<List<String>> data = new List<List<String>>();
+            
+            Regex rgx = new Regex(@"\[.*?\]");
+            foreach (Match match in rgx.Matches(rawData))
+            {
+                String tmp = match.Value.Substring(1, match.Value.Length - 2);
+                List<String> vals = tmp.Split(',').ToList<String>();
+
+                if (Convert.ToDouble(vals[0]) < 10)
+                {
+                    data.Add(vals);
+                }
+            }
+            
+            
+
+            return data;
         }
                
         
